1202c6c83a37a267bb8fcbecffca6259d5c6cbb1
Demi Oshin <demiaoshin@gmail.com>
2022-12-04 23:33:30 +0000

    improved day 3 using set theory

--- 2022/Sources/2022/day3/day3.swift
+++ 2022/Sources/2022/day3/day3.swift
@@ -2,137 +2,47 @@ import Foundation
 import util
 
 class day3 {
-    let file = "/input.txt"
-    var values = [String]()
-
-    let lowercase = "abcdefghijklmnopqrstuvwxyz"
-    let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-    var lowercaseP = [Character: Int]()
-    var uppercaseP = [Character: Int]()
+    let file = "/Sources/2022/day3/input.txt"
+    let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var charP = [Character: Int]()
+    var rucksacks = [String]()
 
     init() {
         self.setup()
         let path = FileManager.default.currentDirectoryPath + file
-        let rucksacks = util.getIntInput(path: path)
-        self.part1(rucksacks: rucksacks)
-        self.part2(rucksacks: rucksacks)
+        rucksacks = util.getStrInput(path: path)
     }
 
     func setup() {
         var i = 1
-        for char in lowercase {
-            lowercaseP[char] = i
-            i += 1
-        }
-
-        i = 27
-        for char in uppercase {
-            uppercaseP[char] = i
+        for char in chars {
+            charP[char] = i
             i += 1
         }
     }
 
-
-    func part1(rucksacks: [Int]) -> Int {
-        var numberOfRucksacks = rucksacks.count
+    func part1() {
         var total = 0
-        var allchars = [Character]()
-
         
-        // for n in 0...numberOfRucksacks-2 {
-        //     let rucksack = rucksacks[n]
+        for n in 0...rucksacks.count-1 {
+            let commonchar = Array(Set(rucksacks[n].prefix(rucksacks[n].count/2)).intersection(Set(rucksacks[n].suffix(rucksacks[n].count/2))))
 
-        //     let rLength = rucksack.count
+            total += charP[commonchar[0]]!
+        }
 
-        //     let p1 = rucksack.prefix(rLength/2)
-        //     let p2 = rucksack.suffix(rLength/2)
-
-        //     for char in p1 {
-        //         if p2.contains(char) {
-        //             allchars.append(char)
-        //             break
-        //         }
-        //     }
-        // }
-
-        // let stringchars = String(allchars)
-
-        // for char in stringchars {
-        //     if let point = lowercaseP[char] {
-        //         total += point
-        //         print(point)
-        //     } else if let point = uppercaseP[char] {
-        //         total += point
-        //         print(point)
-        //     }
-        // }
-
-        return total
+        print("Part 1: \(total)")
     }
 
-    func part2(rucksacks: [Int]) -> Int {
-        var numberOfRucksacks = rucksacks.count
+    func part2() {
         var total = 0
-        var allchars = [Character]()
 
-        // for n in stride(from: 0, to: numberOfRucksacks-2, by: 3) {
-        //     var rchars = [Character:Int]()
+        for n in stride(from: 0, to: rucksacks.count-2, by: 3) {
+            let commonchar = Array(Set(rucksacks[n]).intersection(Set(rucksacks[n+1])).intersection(Set(rucksacks[n+2])))
 
-        //     let rucksack = Set(rucksacks[n])
-        //     let rucksack1 = Set(rucksacks[n+1])
-        //     let rucksack2 = Set(rucksacks[n+2])
+            total += charP[commonchar[0]]!
+        }
 
-        //     let r1 = Set(rucksack)
-        //     let r2 = Set(rucksack1)
-        //     let r3 = Set(rucksack2)
-
-        //     for char in r1 {
-        //         rchars[char] = 1
-        //     }
-
-        //     for char in r2 {
-        //         if let k = rchars[char] {
-        //             rchars[char] = k+1
-        //         } else {
-        //             rchars[char] = 1
-        //         }
-        //     }
-
-        //     for char in r3 {
-        //         if let k = rchars[char] {
-        //             rchars[char] = k+1
-        //         } else {
-        //             rchars[char] = 1
-        //         }
-        //     }
-
-        //     for char in rchars {
-        //         if char.value == 3 {
-        //             allchars.append(char.key)
-        //             break
-        //         }
-        //     }
-
-
-        //     print(rchars)
-        //     print(allchars)
-        // }
-
-        // let stringchars = String(allchars)
-
-        // for char in stringchars {
-        //     if let point = lowercaseP[char] {
-        //         total += point
-        //     } else if let point = uppercaseP[char] {
-        //         total += point
-        //     }
-        // }
-
-        // print("Final Score: \(total)")
-
-        return total
+        print("Part 2: \(total)")
     }
 }
-
 
