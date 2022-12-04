bc1dfc6ef658a64d0b57187fff8444479d02ed75
Demi Oshin <demiaoshin@gmail.com>
2022-12-04 22:41:08 +0000

    delete old day 1 2021 files

--- 2021/Sources/2021/_021.swift
+++
@@ -1,8 +0,0 @@
-@main
-public struct _021 {
-    public private(set) var text = "Hello, World!"
-
-    public static func main() {
-        print(_021().text)
-    }
-}
--- 2021/day2/day2.swift
+++
@@ -1,5 +0,0 @@
-let file = "/sampleinput.txt"
-var result = ""
-result = try String(contentsOf:  URL(fileURLWithPath: FileManager.default.currentDirectoryPath + file), 
-encoding: .utf8)
-let values = result.components(separatedBy: "\n\n")
