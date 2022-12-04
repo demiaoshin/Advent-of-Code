a2805c6532d95d31047b70459cd0508a3646c38b
Demi Oshin <demiaoshin@gmail.com>
2022-12-04 22:53:22 +0000

    fix day1

--- 2022/Sources/2022/day1/day1.swift
+++ 2022/Sources/2022/day1/day1.swift
@@ -3,41 +3,34 @@ import util
 
 class day1 {
     let file = "/Sources/2022/day1/input.txt"
-    var values = [Int]()
+    var values = [String]()
 
     init() {
         let path = FileManager.default.currentDirectoryPath + file
-        values = util.getIntInput(path: path)
-        self.part1and2()
+        values = util.day1Input(path: path)
     }
 
-    func part1and2() -> [Int] {
-        // var numberOfElves = values.count
-        // var totalElves = [Int]()
-        // var elveSum = 0
+    func part1and2() {
+        let numberOfElves = values.count
+        var totalElves = [Int]()
+        var elveSum = 0
 
-        // print(values)
-
-        // for n in 0...numberOfElves-1 {
-            // print(elve)
-            // let elve = values[n].components(separatedBy: "\n")
+        for n in 0...numberOfElves-1 {
+            let elve = values[n].components(separatedBy: "\n")
         
-            // for item in elve {
-            //     elveSum += item
-            // }
+            for item in elve {
+                elveSum += Int(item) ?? 0
+            }
             
-            // totalElves.append(elveSum)
+            totalElves.append(elveSum)
         
-            // elveSum = 0
-        // }
+            elveSum = 0
+        }
 
-        // print("Highest calorie Elve: \(totalElves.max()!)")
+        print("Highest calorie Elve: \(totalElves.max()!)")
 
-        // totalElves.sort(by: >)
-        // let top3 = totalElves[0] + totalElves[1] + totalElves[2]
-        // print("Top 3 highest calorie elves: \(top3)")
-
-        // return [totalElves.max()!, top3]
-        return [3, 4]
+        totalElves.sort(by: >)
+        let top3 = totalElves[0] + totalElves[1] + totalElves[2]
+        print("Top 3 highest calorie elves: \(top3)")
     }
 }
