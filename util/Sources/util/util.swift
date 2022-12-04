5a171a3dcb1694ceb6de85ff64fc76e80c2c505d
Demi Oshin <demiaoshin@gmail.com>
2022-12-04 23:36:34 +0000

    refactored parsing functions

--- util/Sources/util/util.swift
+++ util/Sources/util/util.swift
@@ -5,20 +5,9 @@ public func getStrInput(path: String) -> [String] {
         let result = try String(contentsOf:  URL(fileURLWithPath: path), encoding: .utf8)
         let rows = result.components(separatedBy: "\n\n")
         var values = rows[0].components(separatedBy: "\n")
-        // get rid of the empty ""
-        values.removeLast()
-        return values
-    } catch {
-        print("error")
-        return []
-    }
-}
-
-public func getSampleStrInput(path: String) -> [String] {
-    do {
-        let result = try String(contentsOf:  URL(fileURLWithPath: path), encoding: .utf8)
-        let rows = result.components(separatedBy: "\n\n")
-        let values = rows[0].components(separatedBy: "\n")
+        if values.last == "" {
+            values.removeLast()
+        }
         return values
     } catch {
         print("error")
@@ -31,22 +20,9 @@ public func getIntInput(path: String) -> [Int] {
         let result = try String(contentsOf:  URL(fileURLWithPath: path), encoding: .utf8)
         let rows = result.components(separatedBy: "\n\n")
         var strvalues = rows[0].components(separatedBy: "\n")
-        // get rid of the empty ""
-        strvalues.removeLast()
-        let intvalues = strvalues.map{ Int($0)!}
-        return intvalues
-    } catch {
-        print("error")
-        return []
-    }
-}
-
-public func getSampleIntInput(path: String) -> [Int] {
-    do {
-        let result = try String(contentsOf:  URL(fileURLWithPath: path), encoding: .utf8)
-        let rows = result.components(separatedBy: "\n\n")
-        let strvalues = rows[0].components(separatedBy: "\n")
-        print(strvalues)
+        if strvalues.last == "" {
+            strvalues.removeLast()
+        }
         let intvalues = strvalues.map{ Int($0)!}
         return intvalues
     } catch {
