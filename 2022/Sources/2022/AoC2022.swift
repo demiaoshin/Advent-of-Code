b3d70b20dd3016d784421d69211eb991848bab4c
Demi Oshin <demiaoshin@gmail.com>
2022-12-04 22:53:35 +0000

    testing for day 1

--- 2022/Sources/2022/AoC2022.swift
+++ 2022/Sources/2022/AoC2022.swift
@@ -3,8 +3,6 @@ public struct AoC2022 {
     public private(set) var text = "Hello, World!"
 
     public static func main() {
-        // change this to the current day
-        print(day4().part1and2())
-        print(day1().part1and2())
+        day1().part1and2()
     }
 }
